a21250b7c25e2f98b98f97cbe483e344857b4694
Jonas Victor <jonas.silva@servicenet.com>
2023-07-31 02:22:18 -0300

    Ajustes no main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-/**
- * Arquivo de inicialização da aplicação
- *
- * Este arquivo configura e inicializa a aplicação Vue.
- * Ele importa os módulos necessários, como o App.vue, o router, o store e as dependências de estilização.
- * Verifica se o usuário está autenticado e redireciona para a tela de login se necessário.
- * Por fim, cria e monta a aplicação Vue.
- */
-
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -32,15 +23,3 @@ createApp(App)
   .use(store)
   .use(router, VueSweetalert2, axios)
   .mount("#app");
-
-// import Vue from "vue";
-// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-// // Import Bootstrap and BootstrapVue CSS files (order is important)
-// import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap-vue/dist/bootstrap-vue.css";
-
-// // Make BootstrapVue available throughout your project
-// Vue.use(BootstrapVue);
-// // Optionally install the BootstrapVue icon components plugin
-// Vue.use(IconsPlugin);
